4d862e0f24b1f78da0eedbe982e06ab703c96a54
lraffa <Tognion@outlook.com>
2015-12-07 19:27:49 +0100

    neue version

--- 09_Praktikum_Bank/src/Start.java
+++ 09_Praktikum_Bank/src/Start.java
@@ -3,11 +3,15 @@ public class Start {
 
 	public static void main(String[] args) {
 		System.out.println(new Bankkonto("test", -1000000));
-		System.out.println(new Salaerkonto("test", -100, 1000));
+		System.out.println(new Salaerkonto("test", -999.95, 1000));
 		System.out.println(new Nummernkonto("test", 100));
 		System.out.println(new Nummernkonto("test", 100));
-//		Salearkonto x = new Salaerkonto();
-		
+		Salaerkonto x = new Salaerkonto("test",-900, 1000);
+		x.einbezahlen(100);	
+		x.abheben(100);
+		x.einbezahlen(1000);
+		System.out.println(x);
+		System.out.println(x.getKontostand());
 
 	}
 
